007fb5309b465b17d52bed7833066728a71a16bb
Rajeev <rajeevkhangar1988@gmail.com>
2024-08-11 19:04:31 +0530

    added specs for class

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+
+  describe '#' do
+    it "does works" do
+      expect(described_class).to be StringCalculator
+    end
+  end
+
+end
